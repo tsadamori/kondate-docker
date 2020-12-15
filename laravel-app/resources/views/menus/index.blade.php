17ec663c9a2b18ddb6740ee2e310d02d2d6ef704
tsadamori <tomoaki.sadamori@gmail.com>
2020-12-15 09:02:53 +0900

    不要なコードを削除

--- laravel-app/resources/views/menus/index.blade.php
+++ laravel-app/resources/views/menus/index.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="mt-5 text-right">
-    {{ link_to_route('menus.create', 'レシピ新規投稿', [], ['class' => 'btn btn-sm btn-primary']) }}
-    {{ link_to_route('kondate.history', '過去の献立', [], ['class' => 'btn btn-sm']) }}
-    {{ link_to_route('logout', 'logout', [], ['class' => 'btn btn-sm btn-secondary']) }}
-</div> -->
+
 @include('menus.kondate')
 
 @include('menus.menus')
--- laravel-app/resources/views/menus/menus.blade.php
+++ laravel-app/resources/views/menus/menus.blade.php
@@ -1,64 +1,40 @@
-    <div>
-        <!-- <div class="form-inline text-right">
-            {!! Form::label('keyword', 'キーワード:') !!}
-            {!! Form::text('keyword', '', ['class' => 'form-control']) !!}
-            {!! Form::label('category1_id', 'カテゴリ:1') !!}
-            {!! Form::select('category1_id', [
-                '' => '選択してください',
-                '1' => '肉',
-                '2' => '卵',
-                '3' => '豆',
-                '4' => '魚',
-                '5' => 'その他',
-            ], ['class' => 'form-control']) !!}
-            {!! Form::label('category2_id', 'カテゴリ2:') !!}
-            {!! Form::select('category2_id', [
-                '' => '選択してください',
-                '1' => '緑',
-                '2' => '豆',
-                '3' => '海藻',
-                '4' => 'きのこ',
-            ], ['class' => 'form-control']) !!}
-            {!! Form::button('search', ['id' => 'search-btn', 'class' => 'form-control btn btn-sm btn-secondary']) !!}
-        </div> -->
-        <div id="menus-list" class="mt-3">
+    <div id="menus-list" class="mt-3">
 @if(count($menus) > 0)
-            <ul class="list-group mb-3">
+        <ul class="list-group mb-3">
 @foreach($menus as $menu)
-                <li class="mb-5">
-                    <div class="row">
-                        <div class="col-7">
-                            <div>
-                                <a href="{{ route('menus.show', $menu->id) }}">
-                                    {{ $menu->name }}
-                                </a>
-                                <span style="font-size: 12px;">　カテゴリ1: {{ $menu->category1_mod }}　カテゴリ2: {{ $menu->category2_mod }}</span>
-                            </div>
-                            <div>
-                                <a href="img/{{ $menu->img_name }}">
-                                    <img class="mt-1" src="img/{{ $menu->img_name }}" height="100">
-                                </a>
-                            </div>
+            <li class="mb-5">
+                <div class="row">
+                    <div class="col-7">
+                        <div>
+                            <a href="{{ route('menus.show', $menu->id) }}">
+                                {{ $menu->name }}
+                            </a>
+                            <span style="font-size: 12px;">　カテゴリ1: {{ $menu->category1_mod }}　カテゴリ2: {{ $menu->category2_mod }}</span>
                         </div>
-                        <div class="menu-btn col-5 text-right">
-                            {!! Form::model($menu, ['route' => ['menus.destroy', $menu->id], 'method' => 'delete']) !!}
-                                {!! Form::button('献立に入れる', [
-                                    'class' => 'add-menu-btn btn btn-sm btn-dark',
-                                    'type' => 'button',
-                                    'data-id' => $menu->id,
-                                    'data-name' => $menu->name
-                                ]) !!}
-                                {!! link_to_route('menus.edit', '編集', [$menu->id], ['class' => 'btn btn-sm btn-dark']) !!}
-                                {!! Form::submit('削除', ['class' => 'btn btn-sm btn-danger']) !!}
-                            {!! Form::close() !!}
+                        <div>
+                            <a href="img/{{ $menu->img_name }}">
+                                <img class="mt-1" src="img/{{ $menu->img_name }}" height="100">
+                            </a>
                         </div>
                     </div>
-                </li>
+                    <div class="menu-btn col-5 text-right">
+                        {!! Form::model($menu, ['route' => ['menus.destroy', $menu->id], 'method' => 'delete']) !!}
+                            {!! Form::button('献立に入れる', [
+                                'class' => 'add-menu-btn btn btn-sm btn-dark',
+                                'type' => 'button',
+                                'data-id' => $menu->id,
+                                'data-name' => $menu->name
+                            ]) !!}
+                            {!! link_to_route('menus.edit', '編集', [$menu->id], ['class' => 'btn btn-sm btn-dark']) !!}
+                            {!! Form::submit('削除', ['class' => 'btn btn-sm btn-danger']) !!}
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </li>
 @endforeach
-            </ul>
-            {{ $menus->links('pagination::bootstrap-4') }}
+        </ul>
+        {{ $menus->links('pagination::bootstrap-4') }}
 @else
-            <p>No contents</p>
+        <p>No contents</p>
 @endif
-        </div>
     </div>
