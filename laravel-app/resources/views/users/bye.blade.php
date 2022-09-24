6d7b227c1593b66ea68a568ba5d3b990d2e8ff70
tsadamori <tomoaki.sadamori@gmail.com>
2022-09-24 10:24:11 +0900

    chore: アプリケーション別リポジトリで管理

--- laravel-app/resources/views/users/bye.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-<div class="pt-3">
-    <p>ご利用ありがとうございました！！！</p>
-</div>
-
-@endsection
--- laravel-app/resources/views/users/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-
-@include('menus.menus', ['menus' => $menus])
-
-@endsection
